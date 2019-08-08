3a8d569d7d0f14e21eeaefba1de555b75231a0f1
Fan Yang <fandulu@users.noreply.github.com>
2019-08-08 11:35:40 +0900

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -9,8 +9,6 @@ import java.lang.reflect.Field;
 /*
  * This code implements the FFT and Random Forests for activity recognition
  * It can be implanted to Android Framework
- * @author Yang Fan
- * @version Jan, 2017
  */
 
 public class Demo
@@ -58,89 +56,73 @@ public class Demo
         }
 
         // Apply FFT 
-        /*
-        FFT fft = new FFT(64); // 64 seconds, sampling rate : 0.1 HZ
-        double[] im = new double[64];
-        double[] re = new double[64];
-        for(int i=0;i<64;i++){
-    			re[i]= data.get(i+500);
-    			im[i]=.0;
-			} 
-
-		fft.fft(re,im);
-		System.out.println("FFT");
-		for(int i=0;i<1+64/2;i++){		
-			Double o = 20*Math.log10(Math.abs(re[i]));
-    			System.out.println(o);
-			} 
-		*/
 		
-		//Data size
-		int n = data.size();
-		//FFT window step
-		int step = 32;
-		//Feature matrix size
-		int rows = n/step-1;
-		int columns = 64/2+1; 
-		//System.out.println(rows);
-		Double [][] features = new Double [rows][columns];
-		List<String> label_prediction = new ArrayList<String>();
-		System.out.println("Prediction label:");
+	//Data size
+	int n = data.size();
+	//FFT window step
+	int step = 32;
+	//Feature matrix size
+	int rows = n/step-1;
+	int columns = 64/2+1; 
+	//System.out.println(rows);
+	Double [][] features = new Double [rows][columns];
+	List<String> label_prediction = new ArrayList<String>();
+	System.out.println("Prediction label:");
 
-		for (int i=0; i < rows; i++){ 
-			// Choose your own windows size base on the data
-			FFT fft = new FFT(64); // window size = 6.4 seconds, sampling rate : 0.1 HZ
-    		double[] im = new double[64];
-    		double[] re = new double[64];
-    		int data_start = step*(i+1)-step;
+	for (int i=0; i < rows; i++){ 
+		// Choose your own windows size base on the data
+		FFT fft = new FFT(64); // window size = 6.4 seconds, sampling rate : 0.1 HZ
+	double[] im = new double[64];
+	double[] re = new double[64];
+	int data_start = step*(i+1)-step;
 
-    		RandomForest rf = new DetectorRandomForest();
-    		
-    		
-    		for(int k=0;k<64;k++){
-				re[k]= data.get(k+data_start);
-				im[k]=.0;
-			}
-			fft.fft(re,im);
-			for (int j=0; j < columns; j++){	
-				double a =  20*Math.log10(Math.abs(re[j]));
-				int b = (int) a;
-				a = (double) b;
-				features[i][j]= a;
-				String fieldName = "f"+(j+1);
-				try
-				{
-					Field field = rf.getClass().getDeclaredField(fieldName);
-					field.setDouble(rf,a);
-				}catch(Exception e){}
-			}
+	RandomForest rf = new DetectorRandomForest();
 
-			Prediction p =rf.runClassification();
-			label_prediction.add(p.label);
-			System.out.println(p.label);
-			
+
+	for(int k=0;k<64;k++){
+			re[k]= data.get(k+data_start);
+			im[k]=.0;
+		}
+		fft.fft(re,im);
+		for (int j=0; j < columns; j++){	
+			double a =  20*Math.log10(Math.abs(re[j]));
+			int b = (int) a;
+			a = (double) b;
+			features[i][j]= a;
+			String fieldName = "f"+(j+1);
+			try
+			{
+				Field field = rf.getClass().getDeclaredField(fieldName);
+				field.setDouble(rf,a);
+			}catch(Exception e){}
 		}
 
+		Prediction p =rf.runClassification();
+		label_prediction.add(p.label);
+		System.out.println(p.label);
 
-		int numTotal = 0;
-		int numCorrect = 0;
-		int label_size = label_true.size();
+	}
 
-		for(int k=0; k<label_size ; k++){
-			boolean correct = false;
-			correct = label_true.get(k).equals(label_prediction.get(k));
-			
-			if (correct) {
-				numCorrect++;
-			}
+
+	int numTotal = 0;
+	int numCorrect = 0;
+	int label_size = label_true.size();
+
+	for(int k=0; k<label_size ; k++){
+		boolean correct = false;
+		correct = label_true.get(k).equals(label_prediction.get(k));
+
+		if (correct) {
+			numCorrect++;
 		}
+	}
 	    int accInt = 10000 * numCorrect / label_size;
 		double acc = accInt / 10000.0;
 		System.out.println(label_size  + ": accuracy: " + acc);
 
 
 		//output the features
-		/*
+
 		try{
 		File file_out = new File ("output.csv"); 
 		PrintWriter pw = new PrintWriter(file_out);
@@ -152,8 +134,8 @@ public class Demo
 		pw.flush();
 		pw.close();	
 		}catch (FileNotFoundException e){
-        	e.printStackTrace();
-        }
-		*/
+		e.printStackTrace();
+		}
+
     }
 }
